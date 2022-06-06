fc334f81e1353f424aa9c71020ab10943d67f3fd
pawaters <86101754+pawaters@users.noreply.github.com>
2022-06-06 11:50:28 +0300

    range

---
+++ 03/ft_range.c
@@ -0,0 +1,18 @@
+int	ft_abs(int n)
+{
+	if (n > 0)
+		n = n * -1;
+	return (n);
+
+int	*ft_range(int start, int end)
+{
+	char *tab;
+
+	tab = malloc(ft_abs(end - start) + 1);
+	if (start > end)
+	{
+		while (end < start)
+			tab[i++] = end++;
+			
+	}
+}	
