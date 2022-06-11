1b968a32f3034e8f806083dee8a95062e011e767
pawaters <86101754+pawaters@users.noreply.github.com>
2022-06-11 10:51:11 +0300

    itoa rewview

---
+++ 04/ft_itoa2.c
@@ -0,0 +1,10 @@
+/*
+ * ft_nbr_len: to malloc the str
+ * ft_div: to give the 10 multiple to divide the int to give the digit to the left
+ *
+ * main loop: 
+ * div := ft_div;
+ * while (nb > 9)
+ * str[i] == (nb / div) + '0';
+ * div /= 10;
+ * i++;  
