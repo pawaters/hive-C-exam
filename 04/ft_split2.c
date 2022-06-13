61ead3dd4a01ea2358045ac74fa17011d88b7263
pawaters <86101754+pawaters@users.noreply.github.com>
2022-06-13 13:09:10 +0300

    summary split2

---
+++ 04/ft_split2.c
@@ -0,0 +1,15 @@
+/* NO NEED TO DIVIDE
+ * 0) define WD_LEN an WD_NUM 1000 (saves work)
+ * 1) malloc char**
+ * 2) pass initial spaces
+ * 3) main loop, with if (str[i] > 32)
+ * 	malloc wd WDLEN
+ * 	create work str
+ * Lesson learned:
+ * - if level 4, no need to divide (30 lines max) (apart from rev_wstr and rostring)
+*/
+
+char    **ft_split(char *str)
+{
+	
+}
