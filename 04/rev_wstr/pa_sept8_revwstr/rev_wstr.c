ece3a052279f3e50a41c62e971f6e0b6945357ae
pawaters <86101754+pawaters@users.noreply.github.com>
2022-09-13 09:29:18 +0300

    rev_wstr tested and working

--- 04/rev_wstr/pa_sept8_revwstr/rev_wstr.c
+++ 04/rev_wstr/pa_sept8_revwstr/rev_wstr.c
@@ -1,3 +1,5 @@
+#include <unistd.h>
+
 int is_white_space(char c)
 {
     return (c == ' ' || c == '\t');
@@ -29,6 +31,8 @@ void print_reverse(char *str)
     {
         //with fast, find start of word
 
+        slow = fast;
+        curr = fast;
         while (fast >= 0 && !is_white_space(str[fast]))
             fast--;
         fast++;
@@ -41,11 +45,18 @@ void print_reverse(char *str)
             curr++;
         }
 
+
         //we wrote the word, add a whitespace, only if not last word
         if (fast > 0)
             write(1, " ", 1);
-
         //jump to last char of previous word:
         fast = fast - 2;
     }
+}
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    print_reverse(argv[1]);
+    return(0);
 }
